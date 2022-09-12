97fb9b86745efa8928010ba3862b83e0c76006f7
Gustavo Santana <gustavossw@hotmail.com>
2022-09-12 19:04:43 -0300

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Header } from "./components/Header/Header";
+import { Hero } from "./components/Hero/Hero";
+import { Offer } from "./components/Offer/Offer";
+
+export const App = () => {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Offer />
+    </>
+  );
+};
